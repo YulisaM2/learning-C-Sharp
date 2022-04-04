6772cf13828dcebc779e10201a6ba314068c84a4
ym2 <yulisa.med2@gmail.com>
2022-04-04 17:10:10 -0500

    Short example on how to use params keyword
    Get minimum value from a list of variable size on function parameter

---
+++ ParamsExample/ParamsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ParamsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int buffer = MinParams(10, 2, 3, 4, 5, 754, 4, -123, -2);
+            Console.WriteLine("The minimum value is : {0}", buffer);
+        }
+
+        // Keyword params used to pass as many arguments as required.
+        public static int MinParams(params int[] list)
+        {
+            int min = int.MaxValue;
+            foreach(int number in list)
+            {
+                if(number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
+        }
+    }
+}
